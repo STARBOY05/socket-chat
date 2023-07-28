0fe641d8390dbcecbbf311ac6c97824877d71969
Alistair Saldanha <shaanalistair47@gmail.com>
2023-07-28 12:08:25 +0530

    Added API Routes

---
+++ chat-app-frontend/src/utils/APIRoutes.js
@@ -0,0 +1,7 @@
+export const host = "http://localhost:5000";
+export const loginRoute = `${host}/api/auth/login`;
+export const registerRoute = `${host}/api/auth/register`;
+export const setAvatarRoute = `${host}/api/auth/setAvatar`;
+export const allUsersRoute = `${host}/api/auth/allUsers`;
+export const addMessageRoute = `${host}/api/messages/addMessage`;
+export const getAllMessagesRoute = `${host}/api/messages/getAllMessages`;
